84801e758447e825f24b2febeae56bf8d5612eb4
Filip <filipporeba967@gmail.com>
2023-09-20 19:35:12 +0200

    first react app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = Math.random();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{text}</p>
+        <button>randomize</button>
       </header>
     </div>
   );
