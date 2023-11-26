34c6f1ffcc63efd7630066becf6fb84837d217e5
Filip <filipporeba967@gmail.com>
2023-11-26 16:57:36 +0100

    wp of menu in Toolbar

--- src/components/CalculatorAppBar.js
+++ src/components/CalculatorAppBar.js
@@ -3,23 +3,39 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import { Container } from '@mui/material';
+import {Box, Button, Container} from '@mui/material';
 import CalculateIcon from '@mui/icons-material/Calculate';
 
+
 export default function CalculatorAppBar() {
-  return (
+    return (
         <AppBar position="fixed">
-          <Toolbar component={Container}>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu">
-              <CalculateIcon sx={{fontSize:45}}/>
-            </IconButton>
-            <Typography variant="h6"sx={{ flexGrow: 1 }}>
-              Geometric Calculator
-            </Typography>
-          </Toolbar>
+            <Toolbar component={Container}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu">
+                    <CalculateIcon sx={{fontSize: 45}}/>
+                </IconButton>
+                <Typography variant="h6" sx={{flexGrow: 1}}>
+                    Geometric Calculator
+                </Typography>
+                <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                    <Button onClick={(x) => {
+                        console.log(x)
+                    }}
+                            sx={{my: 2, color: 'white', display: 'block'}}>
+                        Home
+                    </Button>
+                    <Button onClick={(x) => {
+                        console.log(x)
+                    }}
+                            sx={{my: 2, color: 'white', display: 'block'}}>
+                        Calculator
+                    </Button>
+                </Box>
+            </Toolbar>
         </AppBar>
-  );
+
+    );
 }
