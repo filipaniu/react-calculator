4f6bc95e4ba240dbad2e033d30fd52fe1649ef81
Filip <filipporeba967@gmail.com>
2023-12-14 17:48:55 +0100

    Added gitHubIcon and link to repozytory

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,6 +4,8 @@ import IconButton from "@mui/material/IconButton";
 import CalculateIcon from "@mui/icons-material/Calculate";
 import Typography from "@mui/material/Typography";
 import './HomePage.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import {Link} from 'react-router-dom';
 
 export default function HomePage() {
     return (<Container maxWidth={true} className="welcome">
@@ -14,6 +16,18 @@ export default function HomePage() {
                 </IconButton>
                 <Typography variant="h2" sx={{flexGrow: 1}}>Geometric Calculator</Typography>
             </Grid>
+            <Grid item xs={12}>
+                <Link to="https://github.com/filipaniu/react-calculator">
+                    <IconButton color="primary">
+                        <GitHubIcon sx={{fontSize: 45}}/>
+                    </IconButton>
+                </Link>
+                <Container className="copyRights">
+                    <Typography>Developed by Filip Poreba</Typography>
+                </Container>
+            </Grid>
         </Grid>
+
     </Container>);
+
 }
